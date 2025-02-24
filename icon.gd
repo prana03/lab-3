fbde3137abab1b124c4eadf87eb3c75ef2cc0aee
Prince <160977167+prana03@users.noreply.github.com>
2025-02-24 13:41:57 -0500

    Add files via upload

---
+++ icon.gd
@@ -0,0 +1,13 @@
+extends Sprite2D
+
+
+@export var booleanVar = true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_physical_key_pressed(KEY_RIGHT):
+		position.x += 2
